1a16b7e26345863ade0c3d20f81aa6d57746f738
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2026-01-03 11:16:58 -0300

    Creating services functions to warnings and warning sent_logs

--- back-end/src/services/warning-sent-logs/index.js
+++ back-end/src/services/warning-sent-logs/index.js
@@ -1,18 +1,87 @@
+import prisma from "../../config/prisma";
+
 export const f_create_warning_sent_log = async ({
   user_id,
   warningId,
   channel,
   sent_at,
 }) => {
-  return null;
+  if (!user_id || !warningId || !channel) {
+    throw new Error(
+      "user_id, warningId, and channel are required to create a warning sent log."
+    );
+  }
+  try {
+    const response = await prisma.warning_logs_sent.create({
+      data: {
+        user_id: user_id,
+        warningId: warningId,
+        channel: channel,
+        sent_at: sent_at ? sent_at : new Date(),
+      },
+    });
+    return response;
+  } catch (error) {
+    throw new Error("Error creating warning sent log: " + error.message);
+  }
 };
 
-export const f_delete_warning_log = async () => {};
+export const f_delete_warning_log = async ({ id }) => {
+  if (!id) {
+    throw new Error("id is required to delete a warning sent log.");
+  }
+  try {
+    const response = await prisma.warning_logs_sent.delete({
+      where: {
+        id: id,
+      },
+    });
+    return response;
+  } catch (error) {
+    throw new Error("Error deleting warning sent log: " + error.message);
+  }
+};
 
-export const f_update_warning_log = async () => {};
+export const f_update_warning_log = async ({ id, item_info }) => {
+  if (!id || !item_info) {
+    throw new Error(
+      "id and item_info are required to update a warning sent log."
+    );
+  }
+  try {
+    const response = await prisma.warning_logs_sent.update({
+      where: {
+        id: id,
+      },
+      data: item_info,
+    });
+    return response;
+  } catch (error) {
+    throw new Error("Error updating warning sent log: " + error.message);
+  }
+};
 
-export const f_get_by_id_warning_log = async () => {};
+export const f_get_by_id_warning_log = async ({ id }) => {
+  if (!id) {
+    throw new Error("id is required to delete a warning sent log.");
+  }
+  try {
+    const response = await prisma.warning_logs_sent.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return response;
+  } catch (error) {
+    throw new Error("Error geting by id warning sent log: " + error.message);
+  }
+};
 
 export const f_get_all_warning_sent_logs = async () => {
-    return [];
+  try {
+    const response = await prisma.warning_logs_sent.findMany();
+    return response;
+  } catch (error) {
+    throw new Error("Error geting all warning sent logs: " + error.message);
+  }
 };
--- back-end/src/services/warnings/index.js
+++ back-end/src/services/warnings/index.js
@@ -1,17 +1,67 @@
-export const f_create_warning = async ({title, message}) => {
-    return ''
+import prisma from "../../config/prisma.js";
+
+export const f_create_warning = async ({ status, message }) => {
+  if (!message) {
+    throw new Error("Message is required to create a warning.");
+  }
+  try {
+    const response = await prisma.warnings.create({
+      data: {
+        status: status ? status : "pending",
+        message: message,
+      },
+    });
+    return response;
+  } catch (error) {
+    throw new Error("Error creating warning: " + error.message);
+  }
 };
 
-export const f_delete_warning = async ({id}) => {
-    return ''
+export const f_delete_warning = async ({ id }) => {
+  if (!id) {
+    throw new Error("id is required to create a warning.");
+  }
+  try {
+    const response = await prisma.warnings.delete({
+      where: {
+        id: id,
+      },
+    });
+    return response;
+  } catch (error) {
+    throw new Error("Error deleting warning: " + error.message);
+  }
 };
 
-export const f_update_warning = async ({id, item_info }) => {
-    return ''
+export const f_update_warning = async ({ id, item_info }) => {
+  if (!id || !item_info) {
+    throw new Error("id and item_info is required to create a warning.");
+  }
+  try {
+    const response = await prisma.warnings.update({
+      where: {
+        id: id,
+      },
+      data: item_info,
+    });
+    return response;
+  } catch (error) {
+    throw new Error("Error updating warning: " + error.message);
+  }
 };
 
-export const f_get_by_id_warning = async ({id}) => {
-    return ''
+export const f_get_by_id_warning = async ({ id }) => {
+  if (!id) {
+    throw new Error("id is required to create a warning.");
+  }
+  try {
+    const response = await prisma.warnings.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return response;
+  } catch (error) {
+    throw new Error("Error geting warning by Id: " + error.message);
+  }
 };
-
-export const 
