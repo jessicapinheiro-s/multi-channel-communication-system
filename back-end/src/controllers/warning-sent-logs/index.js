959031c67a744c4af4fafb801e39cc1cd4c519bf
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2026-01-03 11:28:40 -0300

    Completando controler to get warning log by id

--- back-end/src/controllers/warning-sent-logs/index.js
+++ back-end/src/controllers/warning-sent-logs/index.js
@@ -1,6 +1,7 @@
 import {
   f_create_warning_sent_log,
   f_get_all_warning_sent_logs,
+  f_get_by_id_warning_log,
 } from "../../services/warning-sent-logs";
 
 export const create_warning_sent_log = async (req, res) => {
@@ -51,8 +52,9 @@ export const get_waning_by_warning_id = async(req, res) => {
         return res.status(400).json({ error: "Missing warning_id parameter" });
     }
     try {
-
+      const response = await f_get_by_id_warning_log({id: warning_id})
+      return res.status(200).json(response);
     }catch (error) {
-
+      throw new Error("Internal Server Error");
     }
 }
