593d687ef12d8d548221d35d2552022c675be70e
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2026-01-07 14:34:02 -0300

    Add funções para obtenção de informações na inicialização do app

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -6,8 +6,51 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-
+const ambiente = import.meta.env.VITE_AMBIENTE_API;
 const queryClient = new QueryClient();
+
+/*
+queryClient.prefetchQuery({
+  queryKey: ['total_warnings'],
+  queryFn: async () => {
+    const response = await fetch(`${ambiente}/users/get-all`);
+    const total = await response.json();
+    return total.length;
+  }
+});
+queryClient.prefetchQuery({
+  queryKey: ['receptors_registered'],
+  queryFn: async () => {
+    const response = await fetch(`${ambiente}/warnings/get-all`);
+    const total = await response.json();
+    return total.length;
+  }
+});
+queryClient.prefetchQuery({
+  queryKey: ['total_warnings_email'],
+  queryFn: async () => {
+    const response = await fetch(`${ambiente}/warnings/get-all`);
+    const total = await response.json();
+    return total.length;
+  }
+});
+queryClient.prefetchQuery({
+  queryKey: ['total_warnings_meessages'],
+  queryFn: async () => {
+    const response = await fetch(`${ambiente}/warnings/get-all`);
+    const total = await response.json();
+    return total.length;
+  }
+});
+queryClient.prefetchQuery({
+  queryKey: ['total_warnings_logs'],
+  queryFn: async () => {
+    const response = await fetch(`${ambiente}/warnings_logs/get-all`);
+    const total = await response.json();
+    return total.length;
+  }
+});
+*/
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
