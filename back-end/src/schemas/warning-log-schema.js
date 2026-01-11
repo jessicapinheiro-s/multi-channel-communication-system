a960a0441b9e699de4ba12b2870cb9311e76594b
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2026-01-11 17:24:52 -0300

    Criando e implementando o schema de validação de rotas de warning-logs

--- back-end/src/schemas/warning-log-schema.js
+++ back-end/src/schemas/warning-log-schema.js
@@ -4,5 +4,8 @@ export const createWarningSentLogSchema = z.object({
     user_id: z.number(),
     warningId: z.number(),
     channel: z.string(),
-    sent_at: z.date()
 });
+
+export const getByIdWarningSentLogSchema = z.object({
+    id: z.number()
+})
