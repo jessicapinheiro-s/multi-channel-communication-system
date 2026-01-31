b025abe75ff5d46e681da72ef8822b35b92b5013
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2026-01-31 12:23:52 -0300

    Corrigindo ref incorretas que expoem credenciais de acesso importantes.

---
+++ back-end/prisma/seed.js
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const EMAIL = process.env.ADMIN_EMAIL;
+  const PASSWORD = process.env.ADMIN_PASSWORD;
+  const PHONE = process.env.ADMIN_PHONE;
+  const passwordHash = await bcrypt.hash(PASSWORD, 10);
+
+  await prisma.user.create({
+    email: EMAIL,
+    password: passwordHash,
+    phone: PHONE,
+    warnings_preferences: "email",
+    role: "admin",
+  });
+
+  console.log("admin created sucessfully");
+}
+
+main()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
