6b14e77177e6e08836cf26dd3c5220e9cb0f6d8d
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2026-04-19 14:55:47 -0300

    corrigindo typo

--- back-end/src/schemas/user-schema.js
+++ back-end/src/schemas/user-schema.js
@@ -2,8 +2,11 @@ import { normalizeName, normalizePhone } from "../utils/normalize.js";
 import z from "zod";
 
 export const createUserSchema = z.object({
-  name: z.string().min(6, "Nome deve ter pelo menos 3 caracteres").transform(normalizeName),
-  email: z.string().email("Email inválido"),
+  name: z
+    .string()
+    .min(6, "Nome deve ter pelo menos 6 caracteres")
+    .transform(normalizeName),
+  email: z.email("Email inválido"),
   password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
   phone: z.string().optional().transform(normalizePhone),
   user_preferences: z.array(z.string()).optional(),
@@ -11,11 +14,14 @@ export const createUserSchema = z.object({
 
 export const updateUserSchema = z.object({
   id: z.number(),
-  item_data: {
-    name: z.string().min(6, "Nome deve ter pelo menos 3 caracteres").transform(normalizeName),
-    email: z.string().email("Email inválido"),
+  item_data: z.object({
+    name: z
+      .string()
+      .min(6, "Nome deve ter pelo menos 6 caracteres")
+      .transform(normalizeName),
+    email: z.email("Email inválido"),
     password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
     phone: z.string().optional().transform(normalizePhone),
     user_preferences: z.array(z.string()).optional(),
-  },
+  }),
 });
